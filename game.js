fb0672a6fdc9ec62595b33f5b9d6158ce59bdf05
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-06-16 22:27:26 +0500

    Number Guessing Game!

--- game.js
+++ game.js
@@ -1,48 +1,52 @@
-let userConfirm = confirm("DO you want to play the game?");
 
-if (userConfirm) {
+let randumNum = Math.ceil((Math.random() * 100));
+let count = 0;
 
-    let ranNum = Math.ceil((Math.random() * 100));
+let chances = document.getElementById('chances');
+let result = document.getElementById('result');
+let btn = document.querySelector('button')
 
-    let count = 0;
+btn.addEventListener("click", () => {
 
-    while (count < 3) {
+    let userInput = document.getElementById('user').value;
 
-        let userProm = parseInt(prompt("Please enter a number."));
+    if (userInput < 1 || userInput > 100) {
 
-        if (userProm == ranNum) {
+        result.innerText = "Please enter a number between 1 and 100.";
+        chances.innerText = "Your Number Wrong!";
+        return;
 
-            alert("Wow, you’re a legend! You guessed it like a pro!");
+    }
+    let userNumber = userInput;
 
-            break;
+    if (userNumber == randumNum) {
+        result.innerText = "Wow, you’re a legend! You guessed it like a pro!";
+        chances.innerText = "You Win";
+        return;
+    }
 
-        }
+    let absoluteNumber = Math.abs(userNumber - randumNum)
 
-        let absoluteNumber = Math.abs(userProm - ranNum)
+    if (userNumber > randumNum && absoluteNumber <= 5) {
 
-        if (userProm > ranNum && absoluteNumber <= 2) {
+        result.innerText = "Oops! A little too high. Try going a bit lower!";
 
-            alert("Oops! A little too high. Try going a bit lower!");
+    } if (userNumber < randumNum && absoluteNumber <= 5) {
 
-        } else if (userProm < ranNum && absoluteNumber <= 2) {
+        result.innerText = "Oops! A little too low. Try going a bit higher!";
 
-            alert("Oops! A little too low. Try going a bit higher!");
-        }
+    }
 
-        else {
-            if (count == 0) {
-                alert("Sorry! Wrong number. You have 2 chances left.");
-            } if (count == 1) {
-                alert("Sorry! Wrong number. You have 1 chance left.");
-            } if (count == 2) {
-                alert("Sorry! Wrong number. No chances left.");
-            }
-        }
-        count++
-    };
-    if (count == 3) {
-        alert(`Game over! The correct number was ${ranNum}. Unfortunately, you had 3 chances to guess.`);
-    };
+    count++
 
-};
+    if (count == 1) {
+        chances.innerText = "Sorry! Wrong number. You have 2 chances left.";
 
+    } if (count == 2) {
+        chances.innerText = "Sorry! Wrong number. You have 1 chance left.";
+
+    } if (count == 3) {
+        chances.innerText = `Game over! The correct number was ${randumNum}. \n Unfortunately,  you had 3 chances to guess.`;
+    }
+
+});
